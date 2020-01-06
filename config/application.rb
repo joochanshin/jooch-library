afbf44eba670eb40098283daf252972891fa1ddd
JooChan <14jshin@gmail.com>
2020-01-05 22:32:31 -0500

    done

--- config/application.rb
+++ config/application.rb
@@ -17,19 +17,10 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
 module JoochLibrary
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-      config.load_defaults 6.0
-      config.generators do |g|
-      g.test_framework  false
-      g.stylesheets     false
-      g.javascripts     false
-      g.helper          false
-      g.channel         assets: false
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
